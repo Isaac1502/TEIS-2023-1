d85c96d783ad697754da8c065fe98f9c0a59d8ea
Isaac <isaactadinagiraldo02@gmail.com>
2023-02-14 16:10:30 -0500

    arrays, OOP introduction

--- php_cookbook/arrays.php
+++ php_cookbook/arrays.php
@@ -29,10 +29,62 @@ $flarr = range(1, 5, 0.5);
 //unset() --> delete from an array (retain the numeric index)
 //array_splice() --> similar to unset() (doen't retain the index, shift the entire array)
 //array_values() --> reindex the given array
+//array_unshift() --> preprend to the array
+
+//str_split() --> string to array
+//join() --> array to string
+
 
 
 
 $array = ['cas' => 'apple', 'b' => 'banana', 25 => 'cola', 'd' => 'donut'];
 print_r(array_slice($array, 0, 3));
+
+//sort($nums, SORT_NUMERIC); rsort()
+//asort() --> retain the actual keys arsort()
+//usort() --> custom sorting
+
+$bonds = [
+    ['first' => 'Sean',    'last' => 'Connery'],
+    ['first' => 'Daniel',  'last' => 'Craig'],
+];
+
+function sorter(array $a, array $b) {
+    return [$a['last'], $a['first']] <=> [$b['last'], $b['first']];
+}
+
+usort($bonds, 'sorter');
+
+
+//applying certain function to each element of the array
+$values = range(2, 5);
+
+array_walk($values, function(&$value, $key) {
+    $value *= $value;
+});
+
+//array_reduce() using key-values
+function array_reduce_assoc(array $arr, callable $callback, mixed $init = null): mixed {
+    
+    return array_reduce(
+        array_keys($arr),
+        function($carry, $item) use ($arr, $callback){
+            return $callback($carry, $arr[$item], $item);
+        },
+        $init
+    );
+}
+
+$nums = [1 => 10, 2 => 10, 3 => 5];
+
+$mult = array_reduce_assoc(
+    $nums,
+    function($carry, $item, $key){
+        return $carry + ($item * $key);
+    },
+    0
+);
+
+print_r($mult);
 
 ?>
