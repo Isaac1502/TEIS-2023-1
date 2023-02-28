f19a939464894c6f4239eb5617f3fa350cfbddc4
Isaac <isaactadinagiraldo02@gmail.com>
2023-02-27 21:48:26 -0500

    tutorial 2 solved

--- laravelcourse/app/Http/Controllers/HomeController.php
+++ laravelcourse/app/Http/Controllers/HomeController.php
@@ -1,8 +1,9 @@
 <?php
- 
+
 namespace App\Http\Controllers;
+
 use Illuminate\View\View;
- 
+
 class HomeController extends Controller
 {
     public function index(): View
@@ -10,6 +11,3 @@ class HomeController extends Controller
         return view('home.index');
     }
 }
-
-
-?>
